293f83589b4cb99624300aa5cfd620d4edf526e9
tanaysubramanian <tanay.subramanian@gmail.com>
2024-02-25 17:57:31 -0500

    REPL Function

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import '../styles/App.css';
-import { LoginButton } from './LoginButton';
-import REPL from './REPL';
+import { useState } from "react";
+import "../styles/App.css";
+import { LoginButton } from "./LoginButton";
+import REPL from "./REPL";
 
 /**
  * This is the highest level component!
@@ -16,7 +16,7 @@ function App() {
         <LoginButton isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
       </p>
 
-      { isLoggedIn && <REPL /> }
+      {isLoggedIn && <REPL />}
     </div>
   );
 }
